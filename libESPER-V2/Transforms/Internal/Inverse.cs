10d6294381b8f1b7167f1fdd2f6307b98ff8aced
CdrSonan <64283097+CdrSonan@users.noreply.github.com>
2025-06-26 22:49:31 +0200

    further work on inverse path

--- libESPER-V2/Transforms/Internal/Inverse.cs
+++ libESPER-V2/Transforms/Internal/Inverse.cs
@@ -12,7 +12,7 @@ internal class Inverse
         var rng = new System.Random(seed);
         var rayleigh = new Rayleigh(scale, rng);
         var output = Vector<float>.Build.Dense(n, 0);
-        for (int i = 0; i < n; i++)
+        for (var i = 0; i < n; i++)
         {
             output[i] = (float)rayleigh.Sample();
         }
@@ -22,27 +22,20 @@ internal class Inverse
     {
         var pitch = audio.GetPitch();
         var amplitudes = audio.GetVoicedAmps();
-        Matrix<float> basis = audio.GetVoicedPhases();
-        var phaseVector = Vector<float>.Build.Dense(audio.Length, phase);
-        for (var i = 1; i < audio.Length; i++)
+        var phases = audio.GetVoicedPhases();
+        var output = Vector<float>.Build.Dense(audio.Length * audio.Config.StepSize, 0);
+        for (var i = 0; i < audio.Config.StepSize / 2; i++)
         {
-            phaseVector[i] = phaseVector[i - 1] + 2 * (float)Math.PI / pitch[i];
+            var components = phases.Row(0) + Vector<float>.Build.Dense(audio.Config.NUnvoiced, (j) => j * 2 * (float)Math.PI / pitch.First());
+            components.MapIndexedInplace((j, value) => amplitudes[0, j] * (float)Math.Cos(value + j * phase));
+            output[i] = components.Sum();
         }
-        basis.MapIndexedInplace((i, j, value) => 
-            amplitudes[i, j] * (float)Math.Cos(value + j * phaseVector[i]));
-        return (basis.ColumnSums(), phaseVector.Last());// TODO calculate output per sample, not frame
+        return (output, 0);
     }
     public static Vector<float> ReconstructUnvoiced(EsperAudio audio, long seed)
     {
         var unvoiced = audio.GetUnvoiced();
-        var output = Vector<float>.Build.Dense(audio.Length, 0);
-        for (var i = 0; i < unvoiced.Count; i++)
-        {
-            var start = unvoiced[i].Start;
-            var end = unvoiced[i].End;
-            if (start < 0 || end >= length) continue;
-            output.Add(x.SubVector(start, end - start + 1));
-        }
+        var output = Vector<float>.Build.Dense(audio.Length * audio.Config.StepSize, 0);
         return output;
     }
 }
